8c33adc676a010ff27c9daede5452691c565e1c8
Shushant Tripathi <shushant1309.com>
2023-11-19 10:34:30 +0000

    Database seeding is working fine

---
+++ database/seeders/CommentTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comment;
+
+class CommentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comment = new Comment();
+        $comment->commentcontent = "Commented on first post";
+        $comment->save();
+    }
+}
---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $post = new Post();
+        $post->postcontent = "My first post";
+        $post->save();
+    }
+}
